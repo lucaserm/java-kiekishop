6eac554ad7c8b9314862f22a4bcc16195901087f
lucaserm <lucasedumacedo@gmail.com>
2025-08-01 19:33:56 -0400

    feat: initial commit

---
+++ microservices/ordering/src/test/java/site/lmacedo/kiekishop/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package site.lmacedo.kiekishop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
